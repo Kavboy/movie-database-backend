0a892997e64398058a79545c43d46dabc225bef1
Kevin <kekait03@hs-esslingen.de>
2022-04-09 13:43:08 +0200

    changes for mariadb docker

--- install.sh
+++ install.sh
@@ -2,26 +2,7 @@ if [ -f "./.installed" ]; then
     exit 0
 fi
 
-echo "PHP Waiting for MS SQL to be finished creating ⏳"
-
-# wait for MSSQL server to finish install
-export STATUS=1
-i=0
-
-while [[ $STATUS -ne 0 ]] && [[ $i -lt 30 ]]; do
-    sleep 3s
-    i=$i+1
-	if [ -f "./docker/mssql/data/.sql-created" ]; then
-	    STATUS=0
-	fi
-done
-
-if [ $STATUS -ne 0 ]; then
-	echo "PHP Container Error: MSSQL SERVER took more than ninety seconds to start up."
-	exit 1
-fi
-
-echo =============== PHP MSSQL STARTED                   ==========================
+echo =============== PHP STARTED                   ==========================
 
 echo =============== PHP Composer Install                ==========================
 composer install --optimize-autoloader --no-dev
